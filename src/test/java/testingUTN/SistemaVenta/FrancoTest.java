136406557fd624c5a8f2dd36e1b14b9628fb5d2a
francofrighetto <68360116+francofrighetto@users.noreply.github.com>
2024-04-18 16:19:14 -0300

    test parametrizado

--- src/test/java/testingUTN/SistemaVenta/FrancoTest.java
+++ src/test/java/testingUTN/SistemaVenta/FrancoTest.java
@@ -3,7 +3,11 @@ package testingUTN.SistemaVenta;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -26,6 +30,23 @@ public class FrancoTest {
 
         // pruebo cambiarlo a negativo
 		productoParaVender.setCantidad(-1f);
+
+        // pruebo con assert true y false
+		// assertTrue(productoParaVender.getCantidad()>=0);
+        assertFalse(productoParaVender.getCantidad()<=0);
+	}
+
+    // igual que el test de arriba, pero con parametros
+    // prueba muchos casos de una
+    @ParameterizedTest
+    @ValueSource(floats = {10f, -5f, -10f,84.2f})
+	void setCantidadNegativaParametrizado(Float distintosValores){
+        // valor inicial positivo
+        Float valorOriginalPsotivo = 5f;
+		productoParaVender.setCantidad(valorOriginalPsotivo);
+
+        // pruebo cambiarlo a negativo
+		productoParaVender.setCantidad(distintosValores);
 
         // pruebo con assert true y false
 		// assertTrue(productoParaVender.getCantidad()>=0);
