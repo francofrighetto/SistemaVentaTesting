34517af4a5a001add470b55a92d9c95353668078
Aldana <aldanavenier@gmail.com>
2024-04-18 17:11:38 -0300

    Test GetTotal Flujo Normal

--- src/test/java/testingUTN/SistemaVenta/AldanaTest.java
+++ src/test/java/testingUTN/SistemaVenta/AldanaTest.java
@@ -1,5 +1,36 @@
 package testingUTN.SistemaVenta;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*  
+    Test sobre clase: producto vendido
+               metodo: getTotal() 
+*/
+
 public class AldanaTest {
-    
+    @Test // Testeamos un flujo normal
+    public void testGetTotal() {
+        // Creamos un objeto tipo Venta para el constructor de ProductoVendido
+        Venta venta = new Venta();
+        
+        // Creamos un objeto ProductoVendido con una cantidad y precio específicos
+        Float cantidad = 5.0f;
+        Float precio = 10.0f;
+        ProductoVendido producto = new ProductoVendido(cantidad, precio, "ProductoTest", "123", venta);
+        
+        // Calculamos el total esperado 
+        Float totalEsperado = cantidad * precio;            // resultado: 50
+        
+        // Obtenemos el total real utilizando el método getTotal() y lo mostramos
+        Float totalReal = producto.getTotal();              // deberia ser 50 tambien
+
+        // Comprobar si el total esperado y el total real son iguales
+        assertEquals(totalEsperado, totalReal);
+    }
+
+
+
+
+
 }
